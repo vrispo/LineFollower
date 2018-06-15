dea41b9b58a9b16df2d0aa79101cd1ce431b1d60
4r35 <borgioli.niccolo@gmail.com>
2018-06-15 21:54:40 +0200

    Corrected motor start procedure
    Created functions to enable and disable motor driver using ENABLE signal through PIN_D7.

--- code.c
+++ code.c
@@ -99,6 +99,7 @@ void backwardright(){
 
 void InitMotors(){
 	/*Pin to lead the motors: Pin12-13 for motor dx - Pin14-15 for sx motor*/
+	/*Pin to enable motor driver: PinD7*/
 	/*
 	 * pin 13-15 bit1 msb
 	 * pin 12-14 bit0 lsb
@@ -112,15 +113,25 @@ void InitMotors(){
 
 	GPIO_InitTypeDef GPIO_InitStructure;
 
-	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15;
+	GPIO_InitStructure.GPIO_Pin=GPIO_Pin_12|GPIO_Pin_13|GPIO_Pin_14|GPIO_Pin_15|GPIO_Pin_7;
 	GPIO_InitStructure.GPIO_Mode=GPIO_Mode_OUT;
 	GPIO_InitStructure.GPIO_OType=GPIO_OType_PP;
 	GPIO_InitStructure.GPIO_Speed=GPIO_Speed_100MHz;
 	GPIO_InitStructure.GPIO_PuPd=GPIO_PuPd_NOPULL;
 	GPIO_Init(GPIOD, &GPIO_InitStructure);
 
+	DisableMotors();
+
 	forwardleft();
 	forwardright();
+}
+
+void EnableMotors(){
+	GPIO_SetBits(GPIOD, GPIO_Pin_7);
+}
+
+void DisableMotors(){
+	GPIO_ResetBits(GPIOD, GPIO_Pin_7);
 }
 
  /*init line sensor*/
@@ -286,6 +297,9 @@ int main(void)
 	InitMotors();
 
 	PWM_Config_and_En();
+
+	//Enable motors
+	EnableMotors();
 
 	/*Start line sensor*/
 	//InitLineSensor();
